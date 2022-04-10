bc8c353bb24fe87755996fc9a3d82b01dcf5b557
McKB <mckennakb@comcast.net>
2022-04-10 17:16:33 -0400

    passes all tests (not xtra cred). Adds partialBuy() function.

--- orderBook.js
+++ orderBook.js
@@ -48,6 +48,7 @@ const readBook = (book, newOrder) => {
 // if both quantity and price match one exisitng order, remove that order from existing book and end the search.
 // if quantity matches but price does not, check the rest, then add the order to book. (add extra credit here)
 // if quantity does not match but price does, more testing is required.
+// if neither price nor quantity match, add the order to the book. (extra credit may apply here? (partial orders))
 const buyOrSell = (existingOrder, newOrder) => {
   let updatesArray = []
 
@@ -55,7 +56,9 @@ const buyOrSell = (existingOrder, newOrder) => {
     return updatesArray
   }
   else if (existingOrder.price === newOrder.price) {
-    // another function here?
+    updatesArray = partialBuy(existingOrder, newOrder)
+
+    return updatesArray
   }
   else {
     updatesArray.push(existingOrder)
@@ -66,11 +69,27 @@ const buyOrSell = (existingOrder, newOrder) => {
 }
 
 // 1. if quantity in existing book is smaller, partially fulfill the incoming order, 
-//      remove that order, keep looking through existing book for another matching price. 
+//      remove the existing order, keep looking through existing book for another matching price. 
 //      If no more matching price, and add remainder of incoming to order book. 
 //      If price matches another, run through the parent if-statement again. 
 // 2. if quantity in existing book is larger, fulfill the incoming order and reduce
 //      the existing order in the book
+const partialBuy = (existingOrder, newOrder) => {
+  let orderArray = []
+  let updatedNewOrder = newOrder
+  let updatedExistingOrder = existingOrder
 
-// if neither price nor quantity match, add the order to the book. (extra credit may apply here? (partial orders))
+  if (existingOrder.quantity < newOrder.quantity) {
+    updatedNewOrder.quantity -= existingOrder.quantity
+    orderArray.push(updatedNewOrder)
+  }
+  else {
+    updatedExistingOrder.quantity -= newOrder.quantity
+    orderArray.push(updatedExistingOrder)
+  }
+
+  return orderArray
+}
+
+
 module.exports = reconcileOrder
